c56684cf60797e155831e46aee29cd690318a598
Minh Anh <cadaik01@gmail.com>
2026-07-01 22:35:18 +0700

    fix file url config và file views system(lỗi đánh máy)

--- backend/system/views.py
+++ backend/system/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
 from .models import AuditLog
-from . serializers import AuditLog
+from .serializers import AuditLogSerializer
 
 class AuditLogViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = AuditLog.objects.all().order_by('-created_at')
